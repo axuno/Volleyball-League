3a1b0254f015806d5344f15deecf020b17c64be2
Norbert Bietsch <nb@axuno.net>
2020-05-02 22:01:36 +0200

    AccountControllerTest now succeeds

--- League.Test/Controllers/AccountControllerTest.cs
+++ League.Test/Controllers/AccountControllerTest.cs
@@ -58,7 +58,7 @@ namespace League.Test.Controllers
             var response = await Client.GetAsync("/");
             response.EnsureSuccessStatusCode();
             var content = await response.Content.ReadAsStringAsync();
-            Assert.IsTrue(content.Contains("volleyball-liga.de"));
+            Assert.IsTrue(content.Contains("logo-league-header.png"));
         }
     }
 }
