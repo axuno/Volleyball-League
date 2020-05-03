9980e12351e30065d405b5917a9b49371be028dc
Norbert Bietsch <nb@axuno.net>
2020-05-03 09:40:07 +0200

    Use OnBeforeEntitySave() instead of Normalize()

--- TournamentManager/DAL/DatabaseGeneric/CustomExtensions/TeamEntity.cs
+++ TournamentManager/DAL/DatabaseGeneric/CustomExtensions/TeamEntity.cs
@@ -7,36 +7,6 @@ namespace TournamentManager.DAL.EntityClasses
 {
 	public partial class TeamEntity
 	{
-		/// <summary>
-		/// Trims all string fields, and sets ':' as a time separator.
-		/// </summary>
-		public void Normalize()
-		{
-			Name = Name.Trim();
-			ClubName = ClubName.Trim();
-		}
-
-		/// <summary>
-		/// Converts the team name property into a string that can be used in an Url.
-		/// All letters and digits are kept as they are, all other characters are replaced with hyphens.
-		/// </summary>
-		/// <returns>Returns the team name as a string that can be used in an Url.</returns>
-		public string UrlSanitizedName()
-		{
-			return UrlSanitizedName(Name);
-		}
-
-		/// <summary>
-		/// Converts the a name into a string that can be used in an Url.
-		/// All letters and digits are kept as they are, all other characters are replaced with hyphens.
-		/// </summary>
-		/// <param name="name">Name to url sanitize.</param>
-		/// <returns>Returns the team name as a string that can be used in an Url.</returns>
-		public static string UrlSanitizedName(string name)
-		{
-			return new string(name.Select(c => Char.IsLetterOrDigit(c) ? c : '-').ToArray()).ToLowerInvariant().Replace("--", "-");
-		}
-
         private DateTime? _dateModifiedCreatedOn;
 
         /// <summary>
@@ -51,6 +21,9 @@ namespace TournamentManager.DAL.EntityClasses
 
         protected override void OnBeforeEntitySave()
         {
+            Name = Name?.Trim();
+            ClubName = ClubName?.Trim();
+
             var now = _dateModifiedCreatedOn ?? DateTime.UtcNow;
             if (IsNew) CreatedOn = now;
             if (IsDirty) ModifiedOn = now;
