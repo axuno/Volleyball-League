fb82ed9c4fa970a107e26c127de2ba51bfde6537
Norbert Bietsch <nb@axuno.net>
2020-05-11 23:10:55 +0200

    Added ViewDataDictionaryExtensions class for title tag and description meta tag

---
+++ League/Helpers/ViewDataDictionaryExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace League.Helpers
+{
+    /// <summary>
+    /// Extension for <see cref="ViewDataDictionary"/> that stores and restores values from it.
+    /// </summary>
+    public static class ViewDataDictionaryExtensions
+    {
+        /// <summary>
+        /// Sets the value used for the page title tag.
+        /// </summary>
+        /// <param name="viewData"></param>
+        /// <param name="title"></param>
+        public static void Title(this ViewDataDictionary<dynamic> viewData, string title)
+        {
+            viewData[nameof(Title)] = title;
+        }
+
+        /// <summary>
+        /// Gets the value used for the page title tag.
+        /// </summary>
+        /// <param name="viewData"></param>
+        /// <returns>Returns the value used for the page title tag</returns>
+        public static string Title(this ViewDataDictionary<dynamic> viewData)
+        {
+            return viewData[nameof(Title)]?.ToString();
+        }
+
+        /// <summary>
+        /// Sets the value used for the page meta description tag.
+        /// </summary>
+        /// <param name="viewData"></param>
+        /// <param name="description"></param>
+        public static void Description(this ViewDataDictionary<dynamic> viewData, string description)
+        {
+            viewData[nameof(Description)] = description;
+        }
+
+        /// <summary>
+        /// Gets the value used for the page meta description tag.
+        /// </summary>
+        /// <param name="viewData"></param>
+        /// <returns>Returns the value used for the page meta description tag</returns>
+        public static string Description(this ViewDataDictionary<dynamic> viewData)
+        {
+            return viewData[nameof(Description)]?.ToString();
+        }
+    }
+}
