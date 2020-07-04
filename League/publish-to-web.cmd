75556101037ffd3eec7a9fcc570517f107635e4f
Norbert Bietsch <nb@axuno.net>
2020-07-04 15:07:24 +0200

    Added EnvironmentName property for publishing

--- League/publish-to-web.cmd
+++ League/publish-to-web.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-dotnet publish -c release -o ..\Web\
+dotnet publish -c release -o ..\Web\ /p:EnvironmentName=Production
 REM dotnet publish -c release -r win81-x86 -o ..\Web\
 REM Without -r parameter: Framework-dependent deployment (FDD), else Self-contained deployment (SCD)
 REM dotnet publish -c release -v detailed -r win81-x86 -o ..\Web\
