1deee0bdabcc4537e5ed7ae7fde01bf5699f69cd
axunonb <axunonb@users.noreply.github.com>
2023-09-02 00:40:07 +0200

    Add extension methods for RoundLegEntity and lists of RoundLegEntity (#111)
    * Implement RoundLegEntity extensions in ModelValidators
    * Match controller: FixtureRuleSet.PlannedMatchTimeMustStayInCurrentLegBoundaries is no more overridden by TournamentEntity.IsPlanningMode
    * Stop team notifications for fixtures when TournamentEntity.IsPlanningMode == true
    * Hotfix: ReportSheet.cshtml handels best-of matches correctly
    * Hotfix in League.Demo: using System.Threading in _Layout.cshtml

---
+++ TournamentManager/TournamentManager/ExtensionMethods/RoundLegEntityListExtensions.cs
@@ -0,0 +1,22 @@
+﻿using TournamentManager.DAL.EntityClasses;
+
+namespace TournamentManager.ExtensionMethods;
+
+/// <summary>
+/// <see cref="RoundLegEntity"/> <see cref="List{T}"/> extensions.
+/// </summary>
+public static class RoundLegEntityListExtensions
+{
+    /// <summary>
+    /// Gets the first <see cref="RoundLegEntity"/> in the <see cref="List{T}"/> that contains the <paramref name="dateTime"/>,
+    /// or <see langword="null"/> if none was found.
+    /// </summary>
+    /// <param name="roundLegList">The <see cref="IList{RoundLegEntity}"/> for which the calculation takes place.</param>
+    /// <param name="dateTime"></param>
+    /// <returns>Returns the first <see cref="RoundLegEntity"/> in the <see cref="List{T}"/> that contains the <paramref name="dateTime"/>,
+    /// or <see langword="null"/> if none was found.</returns>
+    public static RoundLegEntity? GetRoundLegForDate(this IList<RoundLegEntity> roundLegList, DateTime? dateTime)
+    {
+        return roundLegList.FirstOrDefault(roundLeg => roundLeg.ContainsDate(dateTime));
+    }
+}
