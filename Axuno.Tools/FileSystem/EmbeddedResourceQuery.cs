b11e4e43ab69931e1a90869cdeef6895a17f9107
axunonb <nb@axuno.net>
2023-10-29 20:05:10 +0100

    Add EmbeddedResourceQuery class to Axuno.Tools.FileSystem namespace
    Provides methods to read embedded resources from assemblies without using an IFileProvider.

---
+++ Axuno.Tools/FileSystem/EmbeddedResourceQuery.cs
@@ -0,0 +1,85 @@
+﻿using System.Reflection;
+
+namespace Axuno.Tools.FileSystem;
+
+/// <summary>
+/// A class to read embedded resources from assemblies without using an <c>IFileProvider</c>.
+/// </summary>
+public class EmbeddedResourceQuery : IEmbeddedResourceQuery
+{
+    private readonly Dictionary<Assembly, string> _assemblyNames;
+
+    /// <summary>
+    /// Creates a new instance of <see cref="EmbeddedResourceQuery"/>.
+    /// </summary>
+    public EmbeddedResourceQuery() : this(Array.Empty<Assembly>())
+    {
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="EmbeddedResourceQuery"/> with the assemblies to pre-load.
+    /// </summary>
+    /// <param name="assembliesToPreLoad"></param>
+    public EmbeddedResourceQuery(IEnumerable<Assembly> assembliesToPreLoad)
+    {
+        _assemblyNames = new Dictionary<Assembly, string>();
+        foreach (var assembly in assembliesToPreLoad)
+        {
+            TryAddAssemblySimpleName(assembly);
+        }
+    }
+
+    /// <inheritdoc />
+    public Stream? Read<T>(string resource)
+    {
+        var assembly = typeof(T).Assembly;
+        return ReadInternal(assembly, resource);
+    }
+
+    /// <inheritdoc />
+    public Stream? Read(Assembly assembly, string resource)
+    {
+        return ReadInternal(assembly, resource);
+    }
+
+    /// <inheritdoc />
+    public Stream? Read(string assemblyName, string resource)
+    {
+        var assembly = Assembly.Load(assemblyName);
+        return ReadInternal(assembly, resource);
+    }
+
+    internal Stream? ReadInternal(Assembly assembly, string resource)
+    {
+        TryAddAssemblySimpleName(assembly);
+        return assembly.GetManifestResourceStream($"{_assemblyNames[assembly]}.{resource}");
+    }
+
+    private void TryAddAssemblySimpleName(Assembly assembly)
+    {
+        var simpleName = assembly.GetName().Name;
+        if (simpleName != null) _assemblyNames.TryAdd(assembly, simpleName);
+    }
+
+    /// <inheritdoc />
+    public string[] GetResourceNames(Assembly assembly)
+    {
+        TryAddAssemblySimpleName(assembly);
+        return assembly.GetManifestResourceNames();
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<string> GetAllResourceNames()
+    {
+        foreach (var assembly in _assemblyNames.Keys)
+        {
+            var x = assembly.GetFiles();
+            var resourceNames = GetResourceNames(assembly);
+            foreach (var resourceName in resourceNames)
+            {
+                yield return resourceName;
+            }
+        }
+    }
+}
+
---
+++ Axuno.Tools/FileSystem/IEmbeddedResourceQuery.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+
+namespace Axuno.Tools.FileSystem;
+
+/// <summary>
+/// Provides methods to read embedded resources from assemblies without using an <c>IFileProvider</c>.
+/// </summary>
+public interface IEmbeddedResourceQuery
+{
+    /// <summary>
+    /// Reads an embedded resource from the assembly of <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="resource"></param>
+    /// <returns></returns>
+    Stream? Read<T>(string resource);
+
+    /// <summary>
+    /// Reads an embedded resource from the assembly of <paramref name="assemblyName"/>.
+    /// </summary>
+    /// <param name="assembly"></param>
+    /// <param name="resource"></param>
+    /// <returns></returns>
+    Stream? Read(Assembly assembly, string resource);
+
+    /// <summary>
+    /// Reads an embedded resource from the assembly of <paramref name="assemblyName"/>.
+    /// </summary>
+    /// <param name="assemblyName"></param>
+    /// <param name="resource"></param>
+    /// <returns></returns>
+    Stream? Read(string assemblyName, string resource);
+
+    /// <summary>
+    /// Gets all resource names from the assembly of <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="assembly"></param>
+    /// <returns></returns>
+    string[] GetResourceNames(Assembly assembly);
+
+    /// <summary>
+    /// Gets all resource names from all pre-loaded assemblies.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerable<string> GetAllResourceNames();
+}
