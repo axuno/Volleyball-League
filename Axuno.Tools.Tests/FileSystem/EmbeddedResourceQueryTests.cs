246ebe071b7bc2edf6589d8f6a5682633e637592
axunonb <axunonb@users.noreply.github.com>
2023-10-29 20:05:55 +0100

    Add EmbeddedResourceQuery class to Axuno.Tools.FileSystem namespace (#121)
    Provides methods to read embedded resources from assemblies without using an IFileProvider.

---
+++ Axuno.Tools.Tests/FileSystem/EmbeddedResourceQueryTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json;
+using NUnit.Framework;
+
+namespace Axuno.Tools.FileSystem.Tests;
+
+[TestFixture]
+public class EmbeddedResourceQueryTests
+{
+    private readonly EmbeddedResourceQuery _sut = new(new[] { typeof(EmbeddedResourceQueryTests).Assembly });
+
+    [Test]
+    public void CanReadEmbeddedResource_Without_PreLoading()
+    {
+        var sut = new EmbeddedResourceQuery();
+        var assembly = typeof(EmbeddedResourceQueryTests).Assembly;
+        using var stream = sut.Read(assembly, "FileSystem.Data.my-json-file.json");
+
+        var jsonDocument = JsonDocument.Parse(stream!);
+        Assert.That(jsonDocument.RootElement.GetProperty("data").GetBoolean(), Is.EqualTo(true));
+    }
+
+    [TestCase("FileSystem.Data.my-json-file.json")]
+    [TestCase("FileSystem.Data.data.json")]
+    public async Task CanReadEmbeddedResource_Generic(string resource)
+    {
+        await using var stream = _sut.Read<EmbeddedResourceQueryTests>(resource);
+
+        var jsonDocument = await JsonDocument.ParseAsync(stream!);
+        Assert.That(jsonDocument.RootElement.GetProperty("data").GetBoolean(), Is.EqualTo(true));
+    }
+
+    [TestCase("FileSystem.Data.my-json-file.json")]
+    [TestCase("FileSystem.Data.data.json")]
+    public async Task CanReadEmbeddedResource_AssemblyAndResource(string resource)
+    {
+        var assembly = typeof(EmbeddedResourceQueryTests).Assembly;
+        await using var stream = _sut.Read(assembly, resource);
+
+        var jsonDocument = await JsonDocument.ParseAsync(stream!);
+        Assert.That(jsonDocument.RootElement.GetProperty("data").GetBoolean(), Is.EqualTo(true));
+    }
+
+    [TestCase("FileSystem.Data.my-json-file.json")]
+    [TestCase("FileSystem.Data.data.json")]
+    public async Task CanReadEmbeddedResource_AssemblyNameAndResource(string resource)
+    {
+        await using var stream = _sut.Read("Axuno.Tools.Tests", resource);
+
+        var jsonDocument = await JsonDocument.ParseAsync(stream!);
+        Assert.That(jsonDocument.RootElement.GetProperty("data").GetBoolean(), Is.EqualTo(true));
+    }
+
+    [Test]
+    public void CanGetResourceNames()
+    {
+        var assembly = typeof(EmbeddedResourceQueryTests).Assembly;
+        var resourceNames = _sut.GetResourceNames(assembly).ToList();
+
+        Assert.That(resourceNames.Count, Is.EqualTo(2));
+        Assert.That(resourceNames, Contains.Item("Axuno.Tools.Tests.FileSystem.Data.my-json-file.json"));
+    }
+
+    [Test]
+    public void CanGetAllResourceNames()
+    {
+        var resourceNames = _sut.GetAllResourceNames().ToList();
+
+        Assert.That(resourceNames.Count, Is.EqualTo(2));
+        Assert.That(resourceNames, Contains.Item("Axuno.Tools.Tests.FileSystem.Data.my-json-file.json"));
+    }
+}
