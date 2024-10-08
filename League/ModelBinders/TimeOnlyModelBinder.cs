a00e010f76fc99df3082b37b95d8e6f1f51f5970
axunonb <axunonb@users.noreply.github.com>
2024-10-08 11:04:54 +0200

    Enhance security for links, cookies, and regex operations
    * Add `rel="noopener"` to various anchor tags across multiple files to prevent access to `window.opener`.
    * Add `Secure` and `HttpOnly` attributes to cookies
    * Introduced `RegexTimeout` constant in `TimeOnlyModelBinder.cs` and `TimeSpanModelBinder.cs`, and updated `Regex.Replace` calls

--- League/ModelBinders/TimeOnlyModelBinder.cs
+++ League/ModelBinders/TimeOnlyModelBinder.cs
@@ -12,6 +12,7 @@ public class TimeOnlyModelBinder : IModelBinder
 {
     private readonly IModelBinder _fallbackBinder;
     private readonly ILogger<TimeOnlyModelBinder> _logger;
+    private const int RegexTimeout = 300;
 
     private const DateTimeStyles _dateTimeStyles = DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AllowLeadingWhite | DateTimeStyles.AllowTrailingWhite;
 
@@ -56,8 +57,8 @@ public class TimeOnlyModelBinder : IModelBinder
     {
         if (text != null)
         {
-            text = Regex.Replace(text, "([^0-9]|^)([0-9])([0-9]{2})([^0-9]|$)", "$1$2:$3$4");
-            text = Regex.Replace(text, "^[0-9]$", "0$0");
+            text = Regex.Replace(text, "([^0-9]|^)([0-9])([0-9]{2})([^0-9]|$)", "$1$2:$3$4", RegexOptions.Compiled, TimeSpan.FromMilliseconds(RegexTimeout));
+            text = Regex.Replace(text, "^[0-9]$", "0$0", RegexOptions.Compiled, TimeSpan.FromMilliseconds(RegexTimeout));
 
             foreach (var format in _formats)
             {
@@ -67,7 +68,7 @@ public class TimeOnlyModelBinder : IModelBinder
                 return true;
             }
         }
-        
+
         time = TimeOnly.MinValue;
         return false;
     }
