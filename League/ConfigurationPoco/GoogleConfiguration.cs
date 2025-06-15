40f6ad7b6634f7fce9fdaf766dc0c1461ebe678b
axunonb <axunonb@users.noreply.github.com>
2025-06-15 11:00:39 +0200

    Google Maps: Use MapId and Extendend Markers
    - Refactored `mapInitializer` function to utilize the new Google Maps API structure with `AdvancedMarkerElement`.
    - Updated map initialization to include the new `MapId` for advanced markers.
    - Add `MapId` to `GoogleConfiguration`
    - Modified `Credentials.json` to include `MapId`.

--- League/ConfigurationPoco/GoogleConfiguration.cs
+++ League/ConfigurationPoco/GoogleConfiguration.cs
@@ -4,6 +4,7 @@ namespace League.ConfigurationPoco;
 public class GoogleConfiguration
 {
     public string WebApiKey { get; set; } = string.Empty;
+    public string MapId { get; set; } = string.Empty;
     public string ServiceApiKey { get; set; } = string.Empty;
     public string AnalyticsTrackingId { get; set; } = string.Empty;
 }
