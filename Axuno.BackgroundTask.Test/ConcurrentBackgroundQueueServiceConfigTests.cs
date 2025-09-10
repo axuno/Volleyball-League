e58d1c7dba21f25aac912212829e10765ae23b94
axunonb <axunonb@users.noreply.github.com>
2025-09-10 13:37:08 +0200

    Set default number of concurrent task from 5 to 50 (#268)
    Remove obsolete `BackggroundTaskEmpty` class

--- Axuno.BackgroundTask.Test/ConcurrentBackgroundQueueServiceConfigTests.cs
+++ Axuno.BackgroundTask.Test/ConcurrentBackgroundQueueServiceConfigTests.cs
@@ -8,11 +8,11 @@ public class ConcurrentBackgroundQueueServiceConfigTests
     [Test]
     public void Set_Config_Default()
     {
-        var config = new ConcurrentBackgroundQueueServiceConfig{ PollQueueDelay = default, MaxConcurrentCount = -1};
-        Assert.Multiple(() =>
+        var config = new ConcurrentBackgroundQueueServiceConfig { PollQueueDelay = TimeSpan.Zero, MaxConcurrentCount = -1 };
+        using (Assert.EnterMultipleScope())
         {
             Assert.That(config.PollQueueDelay, Is.EqualTo(TimeSpan.FromMilliseconds(100)));
-            Assert.That(config.MaxConcurrentCount, Is.EqualTo(5));
-        });
+            Assert.That(config.MaxConcurrentCount, Is.EqualTo(50));
+        }
     }
 }
