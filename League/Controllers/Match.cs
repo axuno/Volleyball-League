axuno/Volleyball-League

commit 173c5467c140f0208e9427beb1fd53f52c06b17c

Fix: Use tournament start date to determine planning mode (#251)

League/Controllers/Match.cs
@@ -831,7 +831,7 @@ public class Match : AbstractController
     private async Task<bool> IsPlanningMode(TournamentEntity tournament, CancellationToken cancellationToken)
     {
         return await _appDb.RoundRepository
-            .GetRoundStartAsync(tournament.Id, cancellationToken)
+            .GetTournamentStartAsync(tournament.Id, cancellationToken)
             .ContinueWith(
                 t => DateTime.UtcNow < t.Result.AddDays(-_tenantContext.SiteContext.MatchNotifications.DaysBeforeNextMatch),
                 TaskContinuationOptions.OnlyOnRanToCompletion);

commit 3110127d91bf707b833f57aa90fa562ef678ed63

Fix: Match notification - `DaysBeforeNextMatch` is a *negativ* number
Correction of bug introduced with #251

League/Controllers/Match.cs
@@ -833,7 +833,8 @@ public class Match : AbstractController
         return await _appDb.RoundRepository
             .GetTournamentStartAsync(tournament.Id, cancellationToken)
             .ContinueWith(
-                t => DateTime.UtcNow < t.Result.AddDays(-_tenantContext.SiteContext.MatchNotifications.DaysBeforeNextMatch),
+                // MatchNotifications.DaysBeforeNextMatch is a negative number or zero
+                t => DateTime.UtcNow < t.Result.AddDays(_tenantContext.SiteContext.MatchNotifications.DaysBeforeNextMatch),
                 TaskContinuationOptions.OnlyOnRanToCompletion);
     }
 }