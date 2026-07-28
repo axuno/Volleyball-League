ba2324db68a3c8f7b5ee36615d6bebfbaf315f3c
axunonb <axunonb@users.noreply.github.com>
2026-07-28 21:30:16 +0200

    Update `RazorViewToStringRenderer` (#309)
    1. Updated RazorViewToStringRenderer.cs
    
    Improvements from `Razor.Templating.Core`:
    • Smart HTTP Context Detection - Automatically uses real HTTP context when available, falls back to default for non-HTTP scenarios (console apps, background workers)
    • Enhanced Error Messages - Provides helpful troubleshooting hints when views are not found
    • Better Routing - Full RouteBuilder setup with custom router
    • IHttpContextAccessor Dependency - Enables HTTP context awareness
    • isMainPage Parameter - Flexibility to specify main vs partial views
    
    2. Created `ViewNotFoundException`
    • Custom exception type in the League.Emailing namespace
    • More specific than generic InvalidOperationException
    • Better for catching view-related errors
    
    3. Original Features Preserved:
    • League.Emailing Namespace - Correct project namespace
    • public Access Modifier - Can be used throughout your application
    • Generic <TModel> Method - Strong typing for type safety
    • EmptyModelMetadataProvider - Explicit metadata provider
    • DisplayName Set - Added to ActionDescriptor for ViewLocalizer compatibility

--- League/Emailing/RazorViewToStringRenderer.cs
+++ League/Emailing/RazorViewToStringRenderer.cs
@@ -1,40 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿/* Copyright (c) .NET Foundation. All rights reserved.
+   Licensed under the Apache License, Version 2.0, http://www.apache.org/licenses/LICENSE-2.0. See License.txt in the project root for license information.
+   Source: https://github.com/aspnet/Entropy/blob/master/samples/Mvc.RenderViewToString/RazorViewToStringRenderer.cs
+   Enhanced with improvements from Razor.Templating.Core (https://github.com/soundaranbu/Razor.Templating.Core)
+   Original code modified by axuno. Modifications Copyright (c) by axuno.
+*/
+
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
-/* Copyright (c) .NET Foundation. All rights reserved.
-   Licensed under the Apache License, Version 2.0, http://www.apache.org/licenses/LICENSE-2.0. See License.txt in the project root for license information.
-   Source: https://github.com/aspnet/Entropy/blob/master/samples/Mvc.RenderViewToString/RazorViewToStringRenderer.cs
-   Original code modified by axuno. Modifications Copyright (c) by axuno.
-
-   See also a Controller extension:
-   https://github.com/RickStrahl/WestwindToolkit/blob/master/Westwind.Web.Mvc/Utils/ViewRenderer.cs   (MIT License)
-*/
-
 namespace League.Emailing;
 
-public class RazorViewToStringRenderer
+public class RazorViewToStringRenderer(
+    IRazorViewEngine viewEngine,
+    ITempDataProvider tempDataProvider,
+    IServiceProvider serviceProvider,
+    IHttpContextAccessor httpContextAccessor)
 {
-    private readonly IRazorViewEngine _viewEngine;
-    private readonly ITempDataProvider _tempDataProvider;
-    private readonly IServiceProvider _serviceProvider;
-
-    public RazorViewToStringRenderer(
-        IRazorViewEngine viewEngine,
-        ITempDataProvider tempDataProvider,
-        IServiceProvider serviceProvider)
-    {
-        _viewEngine = viewEngine;
-        _tempDataProvider = tempDataProvider;
-        _serviceProvider = serviceProvider;
-    }
-
-    public async Task<string> RenderViewToStringAsync<TModel>(string viewName, TModel model)
+    /// <summary>
+    /// Renders a Razor view to a string with strong typing support.
+    /// </summary>
+    /// <typeparam name="TModel">The type of the model.</typeparam>
+    /// <param name="viewName">The name or path of the view to render.</param>
+    /// <param name="model">The model to pass to the view.</param>
+    /// <param name="isMainPage">Whether this is a main page (true) or partial (false).</param>
+    /// <returns>The rendered view as a string.</returns>
+    public async Task<string> RenderViewToStringAsync<TModel>(string viewName, TModel model, bool isMainPage = true)
     {
         var actionContext = GetActionContext();
-        var view = FindView(actionContext, viewName);
+        var view = FindView(actionContext, viewName, isMainPage);
 
         await using var output = new StringWriter();
         var viewContext = new ViewContext(
@@ -42,30 +39,30 @@ public class RazorViewToStringRenderer
             view,
             new ViewDataDictionary<TModel>(
                 metadataProvider: new EmptyModelMetadataProvider(),
-                modelState: new())
+                modelState: new ModelStateDictionary())
             {
                 Model = model
             },
             new TempDataDictionary(
                 actionContext.HttpContext,
-                _tempDataProvider),
+                tempDataProvider),
             output,
-            new());
+            new HtmlHelperOptions());
 
         await view.RenderAsync(viewContext);
 
         return output.ToString();
     }
 
-    private IView FindView(ActionContext actionContext, string viewName)
+    private IView FindView(ActionContext actionContext, string viewName, bool isMainPage)
     {
-        var getViewResult = _viewEngine.GetView(executingFilePath: null, viewPath: viewName, isMainPage: true);
+        var getViewResult = viewEngine.GetView(executingFilePath: null, viewPath: viewName, isMainPage);
         if (getViewResult.Success)
         {
             return getViewResult.View;
         }
-            
-        var findViewResult = _viewEngine.FindView(actionContext, viewName, isMainPage: true);
+
+        var findViewResult = viewEngine.FindView(actionContext, viewName, isMainPage);
         if (findViewResult.Success)
         {
             return findViewResult.View;
@@ -74,14 +71,74 @@ public class RazorViewToStringRenderer
         var searchedLocations = getViewResult.SearchedLocations.Concat(findViewResult.SearchedLocations);
         var errorMessage = string.Join(
             Environment.NewLine,
-            new[] { $"Unable to find view '{viewName}'. The following locations were searched:" }.Concat(searchedLocations));
+            new[] {
+                $"Unable to find view '{viewName}'. The following locations were searched:"
+            }.Concat(searchedLocations)
+            .Concat([
+                "Hint:",
+                "- Check whether you have added reference to the Razor Class Library that contains the view files.",
+                "- Check whether the view file name is correct or exists at the given path."
+            ]));
 
-        throw new InvalidOperationException(errorMessage);
+        throw new ViewNotFoundException(errorMessage);
     }
 
     private ActionContext GetActionContext()
     {
-        var httpContext = new DefaultHttpContext {RequestServices = _serviceProvider};
-        return new(httpContext, new(), new() { DisplayName = nameof(RazorViewToStringRenderer) });
+        var httpContext = httpContextAccessor.HttpContext;
+        var endpoint = httpContext?.GetEndpoint();
+        var actionDescriptor = endpoint?.Metadata.GetMetadata<ActionDescriptor>();
+
+        ActionContext actionContext;
+
+        if (httpContext is null || endpoint is null)
+        {
+            // Non HTTP request scenarios like console, worker services
+            actionContext = GetDefaultActionContext();
+        }
+        else
+        {
+            actionContext = new ActionContext(httpContext, httpContext.GetRouteData(), actionDescriptor ?? new ActionDescriptor());
+        }
+
+        return actionContext;
+    }
+
+    private ActionContext GetDefaultActionContext()
+    {
+        var httpContext = new DefaultHttpContext
+        {
+            RequestServices = serviceProvider
+        };
+        var app = new ApplicationBuilder(serviceProvider);
+        var routeBuilder = new RouteBuilder(app)
+        {
+            DefaultHandler = new CustomRouter()
+        };
+
+        routeBuilder.MapRoute(
+            string.Empty,
+            "{controller}/{action}/{id}",
+            new RouteValueDictionary(new { id = "defaultid" }));
+
+        var actionContext = new ActionContext(httpContext, new RouteData(), new ActionDescriptor
+        {
+            DisplayName = nameof(RazorViewToStringRenderer)
+        });
+        actionContext.RouteData.Routers.Add(routeBuilder.Build());
+        return actionContext;
+    }
+}
+
+internal class CustomRouter : IRouter
+{
+    public VirtualPathData? GetVirtualPath(VirtualPathContext context)
+    {
+        return null;
+    }
+
+    public Task RouteAsync(RouteContext context)
+    {
+        return Task.CompletedTask;
     }
 }
